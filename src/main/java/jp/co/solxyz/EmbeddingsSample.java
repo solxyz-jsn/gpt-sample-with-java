c1a1937e2d2e9f3fde9585e55046fff2fcf22fd2
moritakaho <kaho.morita@solxyz.co.jp>
2025-05-12 10:52:05 +0900

    コンテンツ更新に伴う修正

--- src/main/java/jp/co/solxyz/EmbeddingsSample.java
+++ src/main/java/jp/co/solxyz/EmbeddingsSample.java
@@ -5,35 +5,33 @@ import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.ai.openai.models.EmbeddingItem;
 import com.azure.ai.openai.models.Embeddings;
 import com.azure.ai.openai.models.EmbeddingsOptions;
-import com.azure.ai.openai.models.NonAzureOpenAIKeyCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 
-import java.util.Arrays;
+import java.util.List;
 
 /**
  * Embeddingsの例
  */
 public class EmbeddingsSample {
 
-    /**
-     * OpenAIのAPIキー
-     */
-    private static final String OPENAI_API_KEY = "あなたのアクセスキー";
-
     public static void main(String[] args) {
-        // OpenAIクライアントの生成
+        String url = System.getenv("OPEN_AI_URL");
+        // 1. Azure OpenAI クライアントの生成
         OpenAIClient client = new OpenAIClientBuilder()
-                .credential(new NonAzureOpenAIKeyCredential(OPENAI_API_KEY))
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .endpoint(url)
                 .buildClient();
 
-        // Embeddings対象のテキストを用意
+        // 2. Embeddings対象のテキストを用意
         EmbeddingsOptions embeddingsOptions = new EmbeddingsOptions(
-                Arrays.asList("Hello World"));
+                List.of("Hello World"));
 
-        // モデルを指定してEmbeddingsを行う
-        Embeddings embeddings = client.getEmbeddings("text-embedding-ada-002", embeddingsOptions);
+        // 3. Azure OpenAI サービスでデプロイした埋め込みモデルを指定してEmbeddingsを行う
+        Embeddings embeddings = client.getEmbeddings("jsn-text-embedding-ada-002", embeddingsOptions);
 
+        // 4. レスポンスの表示
         for (EmbeddingItem item : embeddings.getData()) {
-            for (Double embedding : item.getEmbedding()) {
+            for (Float embedding : item.getEmbedding()) {
                 System.out.printf("%f;", embedding);
             }
         }
